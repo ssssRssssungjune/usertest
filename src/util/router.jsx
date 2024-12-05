717033a8618ad01af1ebd4443bfdefe1987e4ef0
Oonet96 <captaing96@gmail.com>
2024-12-05 14:39:22 +0900

    24.12.05 한택 : router.jsx - Navigate import

--- src/util/router.jsx
+++ src/util/router.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import UserPage from "../pages/UserPage";
 import FullScreenPage from "../pages/FullScreenPage";
 import RoomContent from "../components/user/room/script/RoomContent";
