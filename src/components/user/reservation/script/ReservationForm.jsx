53999c7f814d6f7f0e18a9eeaf7c159b21241e84
Oonet96 <captaing96@gmail.com>
2024-12-05 14:40:47 +0900

    24.12.05 한택 : 객실예약화면 작업 진행중

--- src/components/user/reservation/script/ReservationForm.jsx
+++ src/components/user/reservation/script/ReservationForm.jsx
@@ -1,9 +1,47 @@
+import { useOutletContext } from "react-router-dom";
 
 function ReservationForm(){
 
+    const{
+        roomData, 
+        reservationData,
+        selectedTimestamp, 
+        setSelectedTimestamp // function
+    } = useOutletContext();
+
     return(
         <>
-            <div>예약 폼 페이지</div>
+            {/* 미니 달력 / 선택한 날짜 */}
+            <div>
+                <div></div>
+                <div></div>               
+            </div>
+            {/* 객실 선택 */}
+            <div>
+                <div>
+
+                </div>
+                <ul>
+                    {roomData.map((item,index)=>(
+                            <li key={index}>
+                                <p>{item.name}</p>
+                                <p>{item.description}</p>
+                                <p>{item.basePrice}</p>
+                            </li>
+                        )
+                    )}
+                </ul>
+            </div>
+            {/* 예약자 정보입력 / 총 결제금액 안내 */}
+            <div>
+                <div></div>
+                <div></div>
+            </div>
+            {/* 취소 / 예약하기 버튼 */}
+            <div>
+                <button>취소</button>
+                <button>예약하기</button>
+            </div>
         </>
     );
 }
